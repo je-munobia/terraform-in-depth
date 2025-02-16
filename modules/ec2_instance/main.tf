51eeec1aec2c9607322172c3941ce0febe8b393b
Johan Eriksson <info@munobia.se>
2025-02-16 14:15:29 +0100

    feat: Add instance count

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "hello_world" {
+  count         = var.instance_count
   ami           = data.aws_ami.ubuntu.id
   subnet_id     = var.subnet_id
   instance_type = var.instance_type
 
-  tags = merge(var.tags, {Name = "${var.name_prefix}-instance"})
+  tags = merge(var.tags, { Name = "${var.name_prefix}-${count.index}" })
 }
